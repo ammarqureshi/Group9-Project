7cc28a53ec744cc31b02fd8d9598a7948c20995c
Muhammad Ammar Qureshi <qureshm@tcd.ie>
2017-02-22 14:49:48 +0000

    adding packages

--- src/main/java/com/test/Hungarian.java
+++ src/main/java/com/test/Hungarian.java
@@ -1,3 +1,5 @@
+package com.test;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
--- src/main/java/com/test/InputHandel.java
+++ src/main/java/com/test/InputHandel.java
@@ -1,3 +1,5 @@
+package com.test;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
