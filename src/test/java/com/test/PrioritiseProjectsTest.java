248c606a19bb1b090967215dbe8e9386ed46828f
tserlis <erlist@tcd.ie>
2017-03-05 16:25:11 +0000

    Update PrioritiseProjectsTest.java

--- src/test/java/com/test/PrioritiseProjectsTest.java
+++ src/test/java/com/test/PrioritiseProjectsTest.java
@@ -7,54 +7,54 @@ public class PrioritiseProjectsTest {
 
 	@Test
 	public void OnegroupTrue() {
-		String results = "1";
+		String results = "1 1";
 		int[] projects = {1};
 		Assert.assertTrue(PrioritiseProjects.AllAllocated(results, projects));
 	}
 	
 	@Test
 	public void OnegroupFalse() {
-		String results = "1 2 3";
+		String results = "1 1 2 2 3 3";
 		int[] projects = {4};
 		Assert.assertFalse(PrioritiseProjects.AllAllocated(results, projects));
 	}
 	
 	@Test
 	public void FivePriorityProjectsAllocated() {
-		String results = "1 2 3 4 5 6 7 8 9 10";
+		String results = "1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10";
 		int[] projects = {1, 2, 3, 4, 5};
 		Assert.assertTrue(PrioritiseProjects.AllAllocated(results, projects));
 	}
 	
 	@Test
 	public void FivePriorityProjectsNotAllocated() {
-		String results = "1 2 3 4 5 6 7 8 9 10";
+		String results = "1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10";
 		int[] projects = {1, 2, 3, 4, 11};
 		Assert.assertFalse(PrioritiseProjects.AllAllocated(results, projects));
 	}
 	
 	@Test
 	public void OnePriorityProjectPriorityBumped() {
-		String[] groups = new String[]{
-				"1 1 2 3 4 5 6 7 8 9 10 11 12",
-		};
-		String[] newPrefs = new String[]{
-				"1 1 2 3 4 5 6 7 8 9 11 10 12",
-		};
+		String groups = new String(
+				"1 1 2 3 4 5 6 7 8 9 10 11 12"
+		);
+		String newPrefs = new String(
+				"1 1 2 3 4 5 6 7 8 9 11 10 12\n"
+		);
 		int[] projects = {11};
-		Assert.assertArrayEquals(newPrefs, PrioritiseProjects.bumpPriority(groups, projects));
+		Assert.assertEquals(newPrefs, PrioritiseProjects.bumpPriority(groups, projects));
 	}
 	
 	@Test
 	public void FivePriorityProjectPriorityBumped() {
-		String[] groups = new String[]{
-				"1 1 2 3 4 5 6 7 8 9 10 11 12",
-		};
-		String[] newPrefs = new String[]{
-				"1 1 2 3 4 5 7 8 6 10 11 12 9",
-		};
+		String groups = new String(
+				"1 1 2 3 4 5 6 7 8 9 10 11 12"
+		);
+		String newPrefs = new String(
+				"1 1 2 3 4 5 7 8 6 10 11 12 9\n"
+		);
 		int[] projects = {10, 11, 12, 8, 7};
-		Assert.assertArrayEquals(newPrefs, PrioritiseProjects.bumpPriority(groups, projects));
+		Assert.assertEquals(newPrefs, PrioritiseProjects.bumpPriority(groups, projects));
 	}
 	
 	
