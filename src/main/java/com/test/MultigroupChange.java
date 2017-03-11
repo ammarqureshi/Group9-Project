820bd9f07c3630bea89817600e834a3a9e8a3ba6
ConorM16 <muganc@tcd.ie>
2017-03-11 17:55:38 +0000

    Near Final Class,Tests added

--- src/main/java/com/test/MultigroupChange.java
+++ src/main/java/com/test/MultigroupChange.java
@@ -8,39 +8,67 @@ public class MultigroupChange {
 	public static int [][] multiSort(int [][] groups, int [] multiprojs, int [] amountGroups)
 	{
 		newGroups = groups;
-		for(int i = 0; i < multiprojs.length; i++)
+//		for(int i = 0; i < groups.length; i++)
+//		{
+//			for(int j =0; j < groups[i].length; j++)
+//			{
+//				System.out.println("Group: " + groups[i][j]);
+//			}
+//		}
+		
+//		for(int j =0; j < amountGroups.length; j++)
+//		{
+//			System.out.println("Groups per proj : " + amountGroups[j]);
+//		}
+		if(multiprojs.length == amountGroups.length)
 		{
-			projects = assignForProject(groups, multiprojs[i], amountGroups[i]);
-			if(changeMade(groups, newGroups))
+			for(int i = 0; i < multiprojs.length; i++)
 			{
-				for(int j = 0; j < projects.length; j++)
+				//System.out.println("Running for project: " + multiprojs[i]);
+				projects = assignForProject(multiprojs[i], amountGroups[i]);
+	//			if(changeMade(groups, newGroups))
+	//			{
+				if(projects != null)
 				{
-					System.out.println(projects[j] + "   " + (i+1));
+					for(int j = 0; j < projects.length; j++)
+					{
+						System.out.println(multiprojs[i] + "   " + (i+1));
+					}
 				}
+	//				if(newGroups[i]== null)
+	//				{
+	//					System.out.println("newGroups " + i + " = " + newGroups[i]);
+	//				}
+	//			}
+	//			else
+	//				System.out.println("No change made");
 			}
-			else
-				System.out.println("No change made");
+			return newGroups;
 		}
-		return newGroups;
+		else 
+			return null;
 	}
 	
-	public static int [] assignForProject(int [][] groups, int project, int amount)
+	public static int [] assignForProject(int project, int amount)
 	{
-		int choice = 1,count = 0, max = findMax(groups);
+		//System.out.println("Project: " + project + " Amount: " + amount);
+		int choice = 0, max = findMax(newGroups);
 		int [] newProjs  = new int[amount];
 		boolean full = full(newProjs);
 		while(full == false)
-		{	newProjs = assignGroups(groups, newProjs, project,choice,count);
+		{	
+			newProjs = assignGroups(newGroups, newProjs, project,choice);
 			full = full(newProjs);
 			if(full == false)
 			{
-				count++;
+				//count++;
 				choice++;
-			}
-			if (choice > max)
-			{
-				full = true;
-				System.out.println("Error. Could not assign groups");
+				if (choice > max)
+				{
+					full = true;
+					System.out.println("Error. Could not assign groups for project " + project);
+					newProjs = null;
+				}
 			}
 		}
 		return newProjs;
@@ -50,30 +78,52 @@ public class MultigroupChange {
 	{
 		for(int i= 0; i < projs.length; i++)
 		{
+			System.out.println("Checking if full: " + projs[i]);
 			if(projs[i] == 0)
 			{
+				System.out.println("Not full");
 				return false;
 			}
 		}
 		return true;
 	}
 	
-	public static int [] assignGroups(int [][] groups,int [] newProjs, int project,int choice,int count)
+	public static int [] assignGroups(int [][] groups,int [] newProjs, int project,int choice)
 	{
-		System.out.println("Count = " + count);
-		for(int i = 0; i < groups.length; i++)
+		int count =0;
+		//boolean assigned;
+		System.out.println("Size = " + newProjs.length);
+		for(int i = 0; i < newProjs.length; i++)
 		{
-			if(count <= newProjs.length)
+			//System.out.println("Next Project: " + newProjs[i]);
+			//assigned = false;
+			for(int j = 0; j < groups.length; j++)
 			{
-				if((groups[i] != null) && (choice < groups[i].length))
+				if((groups[j] != null)/* && (assigned == false)*/)
 				{
-					if(groups[i][choice] == project)
+					if((choice < groups[j].length) && (newProjs[i] == 0))
 					{
-						addElement(newProjs,count,i);
-						count++;
+						System.out.println("DIS NIGGA CALLED");
+						if(groups[i][choice] == project)
+						{
+							System.out.println("Add element.");
+							addElement(newProjs,i,j+1);
+							//assigned = true;
+						}
 					}
 				}
 			}
+//			if(count <= newProjs.length)
+//			{
+//				if((groups[i] != null) && (choice < groups[i].length))
+//				{
+//					if(groups[i][choice] == project)
+//					{
+//						addElement(newProjs,count,i);
+//						count++;
+//					}
+//				}
+//			}
 		}
 		return newProjs;
 	}
@@ -84,7 +134,9 @@ public class MultigroupChange {
 		{
 			projs[index] = element;
 			newGroups[element] = null;
+			System.out.println("Element added: " + projs[index]);
 		}
+		System.out.println("Element added, list is: " + full(projs));
 		return projs;
 	}
 	
@@ -108,7 +160,7 @@ public class MultigroupChange {
 		int max = 0;
 		for(int i = 0; i < groups.length; i++)
 		{
-			if(groups[i].length >= max)
+			if((groups[i] != null) && (groups[i].length >= max))
 			{
 				max = groups[i].length;
 			}
--- src/test/java/com/test/GreedyMultigroupTest.java
+++ src/test/java/com/test/GreedyMultigroupTest.java
@@ -7,29 +7,33 @@ import org.junit.Test;
 public class GreedyMultigroupTest {
 
 	@Test
-	public void testFourgroups(){
+	public void allFirstPrefs(){
 		int[][] groups = new int[4][5];
 		groups[0] = new int[]{1,2,3,4,5};
 		groups[1] = new int[]{2,3,4,5,6};
-		groups[2] = new int[]{2,4,5,6,7};
+		groups[2] = new int[]{3,4,5,6,7};
 		groups[3] = new int[]{4,5,6,7,8};
-		int [] multiprojs = {1,2,7};
-		int [] amountPerProj = {1,2,1};
+		int [] multiprojs = {1,2,3,10};
+		int [] amountPerProj = {1,1,1,1};
 		
+		int [] expected = null;
 		MultigroupChange multi = new MultigroupChange();
 		groups = multi.multiSort(groups, multiprojs, amountPerProj);
-		int [] expected = null;
-		assertArrayEquals("Expected return is: ",expected, groups[0]);
-		System.out.println("Group 2 = " + groups[1]);
-		assertArrayEquals("Expected return is: ",expected, groups[1]);
-		assertArrayEquals("Expected return is: ",expected, groups[2]);
-		assertArrayEquals("Expected return is: ",expected, groups[3]);
+//		assertArrayEquals("Expected return is: ",expected, groups[0]);
+//		assertArrayEquals("Expected return is: ",null, groups[1]);
+//		assertArrayEquals("Expected return is: ",null, groups[2]);
+//		assertArrayEquals("Expected return is: ",null, groups[3]);
+		assertArrayEquals("Expected return is null",null, groups[0]);
+//		for(int i = 0; i < groups.length; i++)
+//		{
+//			assertArrayEquals("Expected return is: ",null, groups[i]);
+//		}
 	}
 	
 	@Test 
 	public void noneAssigned(){
 		int[][]groups = new int[1][1];
-		groups[0][0] = 1;
+		groups[0] = new int[]{1};
 		int [] multiprojs = {8};
 		int [] amountPerProj = {1};
 		MultigroupChange multi = new MultigroupChange();
@@ -37,6 +41,21 @@ public class GreedyMultigroupTest {
 		groups = multi.multiSort(groups, multiprojs, amountPerProj);
 		assertArrayEquals("Expected return is: ",expected, groups[0]);
 
+	}
+	
+	@Test
+	public void invalidParams(){
+		int[][]groups = new int[3][1];
+		for(int i = 0; i < groups.length; i++)
+		{
+			groups[i][0] = i+1;
+		}
+		int [] multiprojs = {8,2,3,4};
+		int [] amountPerProj = {1,3,4};
+		
+		MultigroupChange multi = new MultigroupChange();
+		groups = multi.multiSort(groups, multiprojs, amountPerProj);
+		assertNull("Expected return is null", groups);
 	}
 }
 
