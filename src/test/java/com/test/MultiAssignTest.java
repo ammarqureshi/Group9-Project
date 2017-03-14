3c58a4194b3c6b8ced4a80f85d6388a02edd6330
tserlis <erlist@tcd.ie>
2017-03-14 13:49:38 +0000

    Update and rename multiGroupTest.java to MultiAssignTest.java

--- src/test/java/com/test/MultiAssignTest.java
+++ src/test/java/com/test/MultiAssignTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class multiGroupTest {
+public class MultiAssignTest {
 
 	@Test
 	public void NoContentionForTwoMP() {
@@ -17,13 +17,13 @@ public class multiGroupTest {
 				+ "10\n"
 				+ "6\n"
 				+ "9\n"
-				+ "8\n"
+				+ "7\n"
 				+ "OwnProjects\n"
 				);
 		
 		String groupPrefs = new String("7\n"
-				+ "1 5 4 10 6 9 8\n"
-				+ "2 5 6 8\n"
+				+ "1 5 4 10 6 9 7\n"
+				+ "2 5 6 7\n"
 				+ "3 2 6\n");
 		String expected = new String("1 5\n"
 				+ "2 5\n"
@@ -33,6 +33,26 @@ public class multiGroupTest {
 
 	}
 	
+	@Test
+	public void oneMPallGroupsChoose() {
+	
+		String projDesc = new String("Projects\n"
+				+ "1\n"
+				+ "1 3\n"		//takes in 3 groups
+				+ "OwnProjects\n"
+				);
+		
+		String groupPrefs = new String("7\n"
+				+ "1 1\n"
+				+ "2 1\n"
+				+ "3 1\n");
+		String expected = new String("1 1\n"
+				+ "2 1\n"
+				+ "3 1\n");
+		String result = Hungarian.getAssignments(groupPrefs, projDesc);
+		assertEquals(expected, result);
+
+	}
 	
 	@Test public void noContentionforThreeMP(){
 		String projDesc = new String("Projects\n"
@@ -66,14 +86,14 @@ public class multiGroupTest {
 	@Test
 	public void contention4choose3MP(){
 	
-	String groupPrefs = new String("4\n"
+	String groupPrefs = new String("5\n"
 			+ "1 5 1 3 2\n"
 			+ "2 5 2 3 1\n"
 			+ "3 5 3 1 2\n"
 			+ "4 5 1 2 3\n");
 	
 	String projDesc = new String("Projects\n"
-			+ "4\n"
+			+ "5\n"
 			+ "1\n"
 			+ "2\n"
 			+ "3\n"
@@ -83,9 +103,9 @@ public class multiGroupTest {
 			);		
 	String expected = new String(
 			"1 5\n"
-			+ "2 2\n"
+			+ "2 5\n"
 			+ "3 5\n"
-			+ "4 5\n");
+			+ "4 1\n");
 	String result = Hungarian.getAssignments(groupPrefs, projDesc);
 	assertEquals(expected, result);
 	}
@@ -125,7 +145,7 @@ public class multiGroupTest {
 	
 	@Test 
 	public void allChooseMP(){
-		String groupPrefs = new String("5/n"
+		String groupPrefs = new String("5\n"
 				+ "1 5 4 1 2\n"
 				+ "2 5 2\n"
 				+ "3 5 2 1 3\n"
@@ -136,6 +156,7 @@ public class multiGroupTest {
 		
 		
 		String projDesc = new String("Projects\n"
+				+ "5\n"
 				+ "1\n"
 				+ "2 3\n" 		//project takes 3 groups
 				+ "3\n"
@@ -145,11 +166,11 @@ public class multiGroupTest {
 		
 		String expected = new String(
 				  "1 4\n"
-				+ "2 2\n"
-				+ "3 5\n"
-				+ "4 2\n"
+				+ "2 5\n"
+				+ "3 2\n"
+				+ "4 3\n"
 				+ "5 2\n"
-				+ "6 3\n"
+				+ "6 2\n"
 				+ "7 1\n"
 				);
 		
