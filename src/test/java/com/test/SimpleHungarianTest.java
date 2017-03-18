8fd87b4a35fac4a86da23dcbf0d8552ebc0d50ad
EziOzoani <eozoani@tcd.ie>
2017-03-18 00:56:10 +0000

    Copywrite stuff + a couple simple tests

--- src/test/java/com/test/SimpleHungarianTest.java
+++ src/test/java/com/test/SimpleHungarianTest.java
@@ -162,9 +162,62 @@ public class SimpleHungarianTest {
 						  "3 3\n";
 		assertEquals(expected, result);
 	}
+	@Test
+	public void withoutGivenGroupNames() {
+		String projectInfo = 
+				"Projects\n" +
+				"1\n" +
+				"2\n" +
+				"3\n" +
+				"4\n" +
+				"5\n" +
+				"6\n" +
+				"7\n" +
+				"8\n" +
+				"9\n" +
+				"10\n" +
+				"11\n" +
+				"12\n" +
+				"13\n" +
+				"14\n" +
+				"15\n" +
+				"16\n";
+		String lastYears =
+			"1 10 11 14\n" +
+			" 2 14 15 9 7 5 11\n" +
+			"3 6 5 4 9 3\n" +
+			" 9 15 5 1 8 4\n" +
+			"5 1 9 3 5 7\n" +
+			" 14 1 3 5 9 7\n" +
+			"7 14 15 7 11 3 13 9\n" +
+			" 3 9 5 1 2\n" +
+			"9 14 6 9 11 3\n" +
+			" 9 4 1 2 7\n" +
+			"11 9 12 5 16 14 1\n" +
+			"1 14 1 2 8 13\n" +
+			"13 5 9 6 1 12 8 15 4\n" +
+			"1 9 7 6 8 14 15 5 4 16 1 2 3 11 13 10 12\n" +
+			"15 14 7 9 2\n";
+		String result = MainProgram.getAssignments(lastYears, projectInfo);
+		String expected = 
+				"1 10\n" + 
+				" 2\n" +
+				"3 6\n" +
+				" 9\n" +
+				"5 1\n" +
+				" 14\n" +
+				"7 15\n" +
+				" 3\n" +
+				"9 11\n" +
+				" 4\n" +
+				"11 12\n" +
+				"1 13\n" +
+				"13 5\n" +
+				"1 8\n" +
+				"15 7\n";
+		assertEquals(expected, result);
+	}
 	
-
-
 	@Test
 	public void lastYearsPreferences() {
 		String projectInfo = 
@@ -221,6 +274,60 @@ public class SimpleHungarianTest {
 		assertEquals(expected, result);
 	}
 	
-
+	@Test
+	public void lastYearsPreferencesWithWeirdGroupNames() {
+		String projectInfo = 
+				"Projects\n" +
+				"1\n" +
+				"2\n" +
+				"3\n" +
+				"4\n" +
+				"5\n" +
+				"6\n" +
+				"7\n" +
+				"8\n" +
+				"9\n" +
+				"10\n" +
+				"11\n" +
+				"12\n" +
+				"13\n" +
+				"14\n" +
+				"15\n" +
+				"16\n";
+		String lastYears =
+			"itty 10 11 14\n" +
+			"2 2 14 15 9 7 5 11\n" +
+			"<3 6 5 4 9 3\n" +
+			"4babyz 9 15 5 1 8 4\n" +
+			"options 1 9 3 5 7\n" +
+			"bitty 14 1 3 5 9 7\n" +
+			"-1 14 15 7 11 3 13 9\n" +
+			"thanks 3 9 5 1 2\n" +
+			"9 14 6 9 11 3\n" +
+			"10 9 4 1 2 7\n" +
+			"spider 9 12 5 16 14 1\n" +
+			":) 14 1 2 8 13\n" +
+			"chancers 5 9 6 1 12 8 15 4\n" +
+			"squad 9 7 6 8 14 15 5 4 16 1 2 3 11 13 10 12\n" +
+			"group 14 7 9 2\n";
+		String result = MainProgram.getAssignments(lastYears, projectInfo);
+		String expected = 
+				"itty 10\n" + 
+				"2 2\n" +
+				"<3 6\n" +
+				"4babyz 9\n" +
+				"options 1\n" +
+				"bitty 14\n" +
+				"-1 15\n" +
+				"thanks 3\n" +
+				"9 11\n" +
+				"10 4\n" +
+				"spider 12\n" +
+				":) 13\n" +
+				"chancers 5\n" +
+				"squad 8\n" +
+				"group 7\n";
+		assertEquals(expected, result);
+	}
 
 }
