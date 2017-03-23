3c5eefbda96b87db0b9b0af282097cff3136733a
Ammar Qureshi <qureshm@tcd.ie>
2017-03-23 20:43:01 +0000

    Update PriorityProjectConstraintTest.java

--- src/test/java/com/test/PriorityProjectConstraintTest.java
+++ src/test/java/com/test/PriorityProjectConstraintTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class PriorityProjectConstraintTests {
+public class PriorityProjectConstraintTest {
 
 	@Test
 	public void simple() {
